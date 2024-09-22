1c4b6c26312724453c025a4cc89c8b0fed9c2bf3
Daniel Casanova <119464431+danielcasanova12@users.noreply.github.com>
2024-09-22 12:07:52 -0300

    feat: models.pt

--- mainTraining.py
+++ mainTraining.py
@@ -44,186 +44,186 @@ transformacoes_modelo2 = {
 
 ###   Modelo 1 AFFECTENET
 
-## Modelo 1 - dados puros
-dataset_path = r'./data/affectnet/processed/imagens_processed'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# ## Modelo 1 - dados puros
+# dataset_path = r'./data/affectnet/processed/imagens_processed'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1affectnet.pt'
-patience = 5
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1affectnet.pt'
+# patience = 5
 
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-## Modelo 1 - Essenciais
-dataset_path = r'./data/affectnet/processed/essenciais'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# ## Modelo 1 - Essenciais
+# dataset_path = r'./data/affectnet/processed/essenciais'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1affectnetEssenciais.pt'
-patience = 5
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1affectnetEssenciais.pt'
+# patience = 5
 
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-## Modelo 1 - landmasks
-dataset_path = r'./data/affectnet/processed/landmarks'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# ## Modelo 1 - landmasks
+# dataset_path = r'./data/affectnet/processed/landmarks'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1affectnetlandmasks.pt'
-patience = 5
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1affectnetlandmasks.pt'
+# patience = 5
 
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-## Modelo 1 - landmarksConecteds
-dataset_path = r'./data/affectnet/processed/landmarksConected'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# ## Modelo 1 - landmarksConecteds
+# dataset_path = r'./data/affectnet/processed/landmarksConected'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1affectnetlandmarksConecteds.pt'
-patience = 5
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1affectnetlandmarksConecteds.pt'
+# patience = 5
 
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
-
-
-###   Modelo 1 FER
-
-## Modelo 1 - dados puros
-dataset_path = r'./data/Fer-2013/processed/imagens_processed'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
-
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1Fer.pt'
-patience = 5
-
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
-
-## Modelo 1 - Essenciais
-dataset_path = r'./data/Fer-2013/processed/essenciais'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
-
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1FerEssenciais.pt'
-patience = 5
-
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
-
-## Modelo 1 - landmasks
-dataset_path = r'./data/Fer-2013/processed/landmarks'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
-
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1Ferlandmasks.pt'
-patience = 5
-
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
-
-## Modelo 1 - landmarksConecteds
-dataset_path = r'./data/Fer-2013/processed/landmarksFullConected'
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
-
-model = ResnetV1(num_classes=num_classes)
-model = model.to(device)
-nameModel = 'model1FerlandmarksConecteds.pt'
-patience = 5
-
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
 
+# ###   Modelo 1 FER
 
+# ## Modelo 1 - dados puros
+# dataset_path = r'./data/Fer-2013/processed/imagens_processed'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-## modelo 2 dados puros
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1Fer.pt'
+# patience = 5
 
-dataset_path = r'./data/affectnet/processed/imagens_processed'
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-# Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# ## Modelo 1 - Essenciais
+# dataset_path = r'./data/Fer-2013/processed/essenciais'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-# Carregar o modelo
-model = CustomResNet50(num_classes).get_model().to(device)
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1FerEssenciais.pt'
+# patience = 5
 
-# Nome do modelo salvo e paciência para early stopping
-nameModel = 'model2affectnet.pt'
-patience = 5
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-# Treinar e validar
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# ## Modelo 1 - landmasks
+# dataset_path = r'./data/Fer-2013/processed/landmarks'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1Ferlandmasks.pt'
+# patience = 5
 
-## modelo 2 essenciais
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
-dataset_path = r'./data/affectnet/processed/essenciais'
+# ## Modelo 1 - landmarksConecteds
+# dataset_path = r'./data/Fer-2013/processed/landmarksFullConected'
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo1)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-# Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# model = ResnetV1(num_classes=num_classes)
+# model = model.to(device)
+# nameModel = 'model1FerlandmarksConecteds.pt'
+# patience = 5
 
-# Carregar o modelo
-model = CustomResNet50(num_classes).get_model().to(device)
-
-# Nome do modelo salvo e paciência para early stopping
-nameModel = 'model2affectnetessenciais.pt'
-patience = 5
-
-# Treinar e validar
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
 
 
-## modelo 2 landmarks
 
-dataset_path = r'./data/affectnet/processed/landmarks'
+# ## modelo 2 dados puros
 
-# Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
-data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+# dataset_path = r'./data/affectnet/processed/imagens_processed'
 
-# Carregar o modelo
-model = CustomResNet50(num_classes).get_model().to(device)
+# # Preparar DataLoaders
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
-# Nome do modelo salvo e paciência para early stopping
-nameModel = 'model2affectnetlandmarks.pt'
-patience = 5
+# # Carregar o modelo
+# model = CustomResNet50(num_classes).get_model().to(device)
 
-# Treinar e validar
-trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
-epocas = 30
-trainer.treinar_e_validar(epocas)
+# # Nome do modelo salvo e paciência para early stopping
+# nameModel = 'model2affectnet.pt'
+# patience = 5
+
+# # Treinar e validar
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
+
+
+# ## modelo 2 essenciais
+
+# dataset_path = r'./data/affectnet/processed/essenciais'
+
+# # Preparar DataLoaders
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+
+# # Carregar o modelo
+# model = CustomResNet50(num_classes).get_model().to(device)
+
+# # Nome do modelo salvo e paciência para early stopping
+# nameModel = 'model2affectnetessenciais.pt'
+# patience = 5
+
+# # Treinar e validar
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
+
+
+
+# ## modelo 2 landmarks
+
+# dataset_path = r'./data/affectnet/processed/landmarks'
+
+# # Preparar DataLoaders
+# data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
+# data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
+
+# # Carregar o modelo
+# model = CustomResNet50(num_classes).get_model().to(device)
+
+# # Nome do modelo salvo e paciência para early stopping
+# nameModel = 'model2affectnetlandmarks.pt'
+# patience = 5
+
+# # Treinar e validar
+# trainer = Trainer(model, data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, device, num_classes, patience, nameModel)
+# epocas = 30
+# trainer.treinar_e_validar(epocas)
 
 
 
@@ -232,7 +232,7 @@ trainer.treinar_e_validar(epocas)
 dataset_path = r'./data/affectnet/processed/landmarksConected'
 
 # Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
+data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
 data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
 # Carregar o modelo
@@ -254,7 +254,7 @@ trainer.treinar_e_validar(epocas)
 dataset_path = r'./data/Fer-2013/processed/imagens_processed'
 
 # Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
+data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
 data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
 # Carregar o modelo
@@ -275,7 +275,7 @@ trainer.treinar_e_validar(epocas)
 dataset_path = r'./data/Fer-2013/processed/essenciais'
 
 # Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
+data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
 data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
 # Carregar o modelo
@@ -297,7 +297,7 @@ trainer.treinar_e_validar(epocas)
 dataset_path = r'./data/Fer-2013/processed/landmarks'
 
 # Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
+data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
 data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
 # Carregar o modelo
@@ -319,7 +319,7 @@ trainer.treinar_e_validar(epocas)
 dataset_path = r'./data/Fer-2013/processed/landmarksFullConected'
 
 # Preparar DataLoaders
-data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_personalizadas)
+data_loader_setup = data_loader_setup = DataLoaderSetup(dataset_path,image_size=224,batch_size=32,transformacoes=transformacoes_modelo2)
 data_loader_treino, data_loader_validacao, num_imagens_treino, num_imagens_validacao, num_classes = data_loader_setup.get_data_loaders()
 
 # Carregar o modelo
